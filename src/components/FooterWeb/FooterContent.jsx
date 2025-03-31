45d0414abafc40a8bd3876297785466766614699
binary-boon <the.binary.boon@gmail.com>
2025-03-31 17:57:45 +0530

    footer modification

--- src/components/FooterWeb/FooterContent.jsx
+++ src/components/FooterWeb/FooterContent.jsx
@@ -7,6 +7,7 @@ import { ImFacebook2 } from "react-icons/im"
 import { FaFacebook } from 'react-icons/fa';
 
 
+
 export default function FooterContent() {
   return (
     <div className='bg-[#fffff] py-8 px-12 h-full w-full flex flex-col justify-between'>
@@ -26,9 +27,16 @@ const Section1 = () => {
 
 const Section2 = () => {
     return (
-        <div className='flex justify-between items-end'>
-            <h1 className='text-[9vw] leading-[0.8] mt-10'>Jayesh Photography</h1>
-            <p>©copyright</p>
+        <div className={styles.footerBrand}>
+            <h1 className={styles.footerHeader}>Jayesh Photography</h1>
+            <a href=""><p>Home</p></a>
+            <a href=""><p>About</p></a>
+            <a href=""><p>Contact</p></a>
+            <a href=""><p>Gallery</p></a>
+            <a href=""><p>Cookie Policy</p></a>
+            
+            <p className={styles.copyright}>©copyright 2025, Jayesh Khaturia Photography</p>
+            <a className={styles.developerLink} href="www.binaryboon.com"><p>Developed by - binary boon</p></a>
         </div>
     )
 }
@@ -48,7 +56,7 @@ const Nav = () => {
                 
                 <BiLogoInstagramAlt size={40}/>
                 <ImFacebook2 size={30}/>
-                <ImFacebook2 size={30}/>
+                
                 <GrGoogle size={30}/>
                 <MdMail size={40}/>
 
